5e561e11ae487cd6ff526bafd9068344539ec1c0
janderson rafael <jandersonrafa@gmail.com>
2019-04-18 19:48:02 -0300

    superficie quadriculada

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Scene from './js/Scene'
+// import './css/style.css'
+// Log message to console
+// eslint-disable-next-line no-console
+ReactDOM.render(
+  <Scene />,
+  // eslint-disable-next-line no-undef
+  document.getElementById('root')
+);
+// Needed for Hot Module Replacement
+// eslint-disable-next-line no-undef
+if(module.hot) {
+  module.hot.accept() // eslint-disable-line no-undef  
+}
---
+++ src/js/Scene.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import 'aframe'
+import image from '../img/grid.jpg'
+
+class Scene extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            planeImage: "shader: flat; src: url(" +  image  +"); repeat: 10 10"
+        }
+    }
+    render() {
+        return (
+            <a-scene>
+                <a-entity
+                    geometry="primitive: box"
+                    position="-1 0.5 -3"
+                    rotation="0 45 0"
+                    material="color: #4CC3D9" />
+                <a-entity
+                    geometry="primitive: sphere; radius: 1.25;"
+                    position="0 1.25 -5"
+                    material="color: #EF2D5E" />
+                <a-entity
+                    geometry="primitive: cylinder; radius: 0.5, height: 1.5"
+                    position="1 0.75 -3"
+                    material="color: #FFC65D" />
+                <a-entity
+                    geometry="primitive: plane; width: 4; height: 4"
+                    position="0 0 -4"
+                    rotation="-90 0 0"
+                    scale="3 3 3"
+                    material={this.state.planeImage}
+                    >
+                    </a-entity>
+            </a-scene>
+        );
+    }
+}
+                    //</a-scene>material="color: #7BC8A4" /
+
+export default Scene;
