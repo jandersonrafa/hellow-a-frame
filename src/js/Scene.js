9c6caf8598ca6d11966d92373b8710fa9317e2d4
janderson rafael <jandersonrafa@gmail.com>
2019-05-27 20:45:14 -0300

    control

--- src/js/Scene.js
+++ src/js/Scene.js
@@ -10,9 +10,29 @@ class Scene extends Component {
     constructor(props) {
         super(props);
 
-        document.addEventListener('keydown', function(event) {
-            debugger
-                alert("key:" + event.key + " - code: " + event.code + " - keyCode: " + event.keyCode)
+        document.addEventListener('keydown', function (event) {
+            let cam = document.querySelector('[camera]');
+            let pos = cam.getAttribute('position');
+            //frente
+            if (event.key == 'MediaFastForward') {
+                pos.z--;
+            }
+            //traz
+            if (event.key == 'MediaRewind') {
+                pos.z++;
+            }
+            // esquerda
+            if (event.key == 'MediaTrackPrevious') {
+                pos.x--;
+            }
+            // direita
+            if (event.key == 'MediaTrackNext') {
+                pos.x++;
+            }
+            cam.setAttribute('position', pos);
+
+            //alert("key:" + event.key + " - code: " + event.code + " - keyCode: " + event.keyCode)
+            console.log("key:" + event.key + " - code: " + event.code + " - keyCode: " + event.keyCode)
         }, false);
     }
 
@@ -24,8 +44,8 @@ class Scene extends Component {
                 <Lab />
                 {/* mesas */}
                 <a-entity scale="1 1 1" position="0 0 0"><Stand /></a-entity>
-                <a-entity scale="1 1 1" position="-5 0 0"><Stand /></a-entity>  
-                <a-entity scale="1 1 1" position="5 0 0"><Stand /></a-entity>  
+                <a-entity scale="1 1 1" position="-5 0 0"><Stand /></a-entity>
+                <a-entity scale="1 1 1" position="5 0 0"><Stand /></a-entity>
                 {/* pcs */}
                 <a-entity position="0 1 0"><Pc /></a-entity>
                 <a-entity position="0 1 3"><Pc /></a-entity>
@@ -36,7 +56,7 @@ class Scene extends Component {
                 <a-entity position="-5 1 3"><Pc /></a-entity>
                 <a-entity position="-5 1 -3"><Pc /></a-entity>
 
-               <Monitor />
+                <Monitor />
             </a-scene>
         );
     }
