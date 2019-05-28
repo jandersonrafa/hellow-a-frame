0faa8acd1fef106b186bca673dbf773067025de6
janderson rafael <jandersonrafa@gmail.com>
2019-05-27 21:44:22 -0300

    test move mobile

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Scene from './js/Scene'
 import './css/style.css'
 // Log message to console
